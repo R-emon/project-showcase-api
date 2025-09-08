8aeb8dc8153b26bc2139ade829b6dfc904c77c6c
Rabiul Islam Emon <rabiulislamemon1@gmail.com>
2025-09-08 23:26:03 +0600

    feat: Add User class and enabled spring SecurityConfig

---
+++ src/main/java/com/showcase/api/model/User.java
@@ -0,0 +1,22 @@
+package com.showcase.api.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String username;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+}
