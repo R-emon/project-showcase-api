8e623e2ecf23284433dd9f930e25aabee0068d2e
Rabiul Islam Emon <rabiulislamemon1@gmail.com>
2025-09-08 13:38:28 +0600

    feat: Add POST endpoint to create projects

--- src/main/java/com/showcase/api/controller/ProjectController.java
+++ src/main/java/com/showcase/api/controller/ProjectController.java
@@ -4,9 +4,8 @@ package com.showcase.api.controller;
 import com.showcase.api.model.Project;
 import com.showcase.api.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,12 @@ public class ProjectController {
     @GetMapping
     public List<Project> getAllProject(){
         return projectRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Project createProject(@RequestBody Project project){
+        return projectRepository.save(project);
     }
 
 }
